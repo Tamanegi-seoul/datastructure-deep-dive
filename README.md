314fdb342cb4b2010f1d2de8b9d993860d1a7a1a
Joonwoo Park <93jpark@gmail.com>
2023-03-01 18:13:57 +0900

    project init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # datastructure-deep-dive
 자료구조 뿌수기
+
+
+
+## 진행순서
+
+
+1. 노드 Node 
+
+2. 리스트 List
+
+3. 스택 Stack
+
+4. 큐 Queue
+
+5. 연결리스트 Linked List
+
+6. 딕셔너리 Dictionary
+
+7. 집합 Set
+
+8. 이진트리 Binary Tree
+
+9. TBA
---
+++ ds/node/exmaple.js
@@ -0,0 +1,14 @@
+
+// 클래스형 Node 객체 정의
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// 함수형 Node 객체 정의
+function Node(data) {
+    this.data = data;
+    this.next= null;
+}
